3eaf31b807e95f70257e5838441d0a01cb6f2fa8
David Hitchen <hitchen28963@yahoo.com>
2024-02-16 23:24:19 +0000

    removed api and added imports for getpass, json, locale and datetime as I will add log in ability for each user

--- run.py
+++ run.py
@@ -1,19 +1,35 @@
-import gspread
-from google.oauth2.service_account import Credentials
+import getpass
+import json
+import locale
+from datetime import datetime, date
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+class DateEncoder(json.JSONEncoder):
+  def default(self, obj):
+      """
+      This class is an extension of json.JSONEncoder.
+      This customizes the default method to enable specific serialization for date objects.
+      During the JSON encoding process, date objects are transformed into their ISO format representation.
+      """
+      if isinstance(obj, date):
+          return obj.isoformat()  # Serialize date objects to ISO format
+      return super().default(obj)
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('penny_provisions')
-
-Expenditure = SHEET.worksheet('Expenditure')
-
-data = Expenditure.get_all_values()
-
-print(data)
+class PennyProvisions:
+  CURRENCY_OPTIONS = {
+    '1': ('USD', 'US Dollar'),
+    '2': ('EUR', 'Euro'),
+    '3': ('GBP', 'British Pound Sterling'),
+    '4': ('JPY', 'Japanese Yen'),
+    '5': ('AUD', 'Australian Dollar'),
+    '6': ('CAD', 'Canadian Dollar'),
+    '7': ('CHF', 'Swiss Franc'),
+    '8': ('CNY', 'Chinese Yuan'),
+    '9': ('SEK', 'Swedish Krona'),
+    '10': ('NZD', 'New Zealand Dollar'),
+    '11': ('INR', 'Indian Rupee'),
+    '12': ('BRL', 'Brazilian Real'),
+    '13': ('ZAR', 'South African Rand'),
+    '14': ('COP', 'Colombian Peso'),
+    '15': ('PEN', 'Peruvian Sol'),
+    '16': ('ILS', 'Israeli New Shekel'),
+  }

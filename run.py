3eaf3374d94690b4ad1690b5cbea98f503143282
David Hitchen <hitchen28963@yahoo.com>
2024-02-16 23:27:36 +0000

    added PennyProvisions class and inputted currency options for initial account creation and monetary displays

--- run.py
+++ run.py
@@ -13,3 +13,23 @@ class DateEncoder(json.JSONEncoder):
       if isinstance(obj, date):
           return obj.isoformat()  # Serialize date objects to ISO format
       return super().default(obj)
+
+class PennyProvisions:
+  CURRENCY_OPTIONS = {
+    '1': ('USD', 'US Dollar'),
+    '2': ('EUR', 'Euro'),
+    '3': ('GBP', 'British Pound Sterling'),
+    '4': ('JPY', 'Japanese Yen'),
+    '5': ('AUD', 'Australian Dollar'),
+    '6': ('CAD', 'Canadian Dollar'),
+    '7': ('CHF', 'Swiss Franc'),
+    '8': ('CNY', 'Chinese Yuan'),
+    '9': ('SEK', 'Swedish Krona'),
+    '10': ('NZD', 'New Zealand Dollar'),
+    '11': ('INR', 'Indian Rupee'),
+    '12': ('BRL', 'Brazilian Real'),
+    '13': ('ZAR', 'South African Rand'),
+    '14': ('COP', 'Colombian Peso'),
+    '15': ('PEN', 'Peruvian Sol'),
+    '16': ('ILS', 'Israeli New Shekel'),
+  }
